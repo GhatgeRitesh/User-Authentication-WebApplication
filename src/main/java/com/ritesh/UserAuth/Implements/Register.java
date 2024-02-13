4d381b0460254e7e82b08dfc2441343cc36f8a83
GhatgeRitesh <riteshghatge12345@gmail.com>
2024-02-13 22:33:19 +0530

    making it advance

---
+++ src/main/java/com/ritesh/UserAuth/Implements/Register.java
@@ -0,0 +1,15 @@
+//package com.ritesh.UserAuth.Implements;
+//
+//import com.ritesh.UserAuth.Interfaces.RegisterConfig;
+//import org.springframework.stereotype.Controller;
+//
+//@Controller
+//public class Register implements RegisterConfig {
+//    public Boolean verify_email(){
+//
+//    }
+//    @Override
+//   public String E_Verify(){
+//        return verify_email();
+//   }
+//}
---
+++ src/main/java/com/ritesh/UserAuth/Interfaces/RegisterConfig.java
@@ -0,0 +1,8 @@
+//package com.ritesh.UserAuth.Interfaces;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public interface RegisterConfig {
+//
+//     public String E_Verify();
+//}
